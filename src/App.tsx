41fe3b633f42bb6d2cc1c42ccdf18c9bc8bf6626
cuddles47 <binbill472003@gmail.com>
2025-09-08 01:12:47 +0700

    Update button text and rephrase invitation in App.tsx; change base URL in vite.config.ts

--- src/App.tsx
+++ src/App.tsx
@@ -33,7 +33,7 @@ export default function Page() {
       {yesPressed ? (
         <>
           <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" />
-          <div className="my-4 text-4xl font-bold">WOOOOOO!!! I love you pookie!! ;))</div>
+          <div className="my-4 text-4xl font-bold">WOOOOOO!!! Luv u so muchhh!! ;))</div>
         </>
       ) : (
         <>
@@ -41,7 +41,7 @@ export default function Page() {
             className="h-[200px]"
             src="https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5oosxnpo6k.gif"
           />
-          <h1 className="my-4 text-4xl">May I ask you out for some sweets?</h1>
+          <h1 className="my-4 text-4xl">Would you grant me the pleasure of taking you out?</h1>
           <div className="flex items-center">
             <button
               className={`mr-4 rounded bg-green-500 px-4 py-2 font-bold text-white hover:bg-green-700`}
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/will-you-be-my-valentine",
+  base: "/may-i-ask-you-out",
   plugins: [react()],
+    server: {
+    port: 5713,
+  },
 });
+
