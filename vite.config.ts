f6317edf5574dfd75e8048ff8bd914cc26aa2a05
cuddles47 <binbill472003@gmail.com>
2025-09-22 02:51:46 +0000

    no messages

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,11 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/may-i-ask-you-out",
+  base: "/",
   plugins: [react()],
   server: {
     port: 5713,
-    host: "172.16.10.77", // hoặc host phù hợp của bạn
+    host: "172.16.56.11", // hoặc host phù hợp của bạn
     allowedHosts: ["pre-thuthapdiachi.vnpost.vn"],
   },
 });
